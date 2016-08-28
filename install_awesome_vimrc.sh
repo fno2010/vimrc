2438eb4bfd95b0557fe2bdc34805caed20bdad7b
jensenzhang <jingxuan.n.zhang@gmail.com>
2016-08-28 11:17:10 +0800

    Support neovim config
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -15,4 +15,9 @@ endtry' > ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+# Neovim support
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
+
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
--- install_basic_vimrc.sh
+++ install_basic_vimrc.sh
@@ -2,4 +2,7 @@ cd ~/.vim
 cat ~/.vim/vimrcs/basic.vim > ~/.vimrc
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ${XDG_CONFIG_HOME:=$HOME/.config}
+ln -s ~/.vim $XDG_CONFIG_HOME/nvim
+ln -s ~/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 echo "Installed the Basic Vim configuration successfully! Enjoy :-)"
